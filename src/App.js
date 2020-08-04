3987dfe346f19a64a1760066d9079120b8a2e507
MartinBlack85 <feketemartonbence85@gmail.com>
2020-08-04 18:22:22 +0200

    updating json

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch('https://ancient-eyrie-27454.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -94,7 +94,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://ancient-eyrie-27454.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
